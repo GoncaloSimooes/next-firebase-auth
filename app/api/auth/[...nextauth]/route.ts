0545ffd67cd9b81ff64fa44db7a75af59405dbc9
GoncaloSimooes <goncalovr9@gmail.com>
2024-09-16 18:10:48 +0100

    fix[route.ts]:define GET and POST as handler

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
 
-export default NextAuth(authOptions);
+// Define the handler and export it for both GET and POST requests
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
